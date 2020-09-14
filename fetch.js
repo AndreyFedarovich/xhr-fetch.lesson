135895556499be040c29955d55d8ea7d7eef821f
Andrey Fedorovich <a.fedorovich@pixelplex.by>
2020-09-14 23:52:26 +0300

    initial

---
+++ fetch.js
@@ -0,0 +1,42 @@
+const requestUrl = 'https://jsonplaceholder.typicode.com/users';
+
+function sendRequest(method, url, body = null) {
+	const postParams = method === 'POST' ? {
+		body: JSON.stringify(body),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	} : {};
+
+	return fetch(url, {
+		method,
+		...postParams
+	}).then(response => {
+		if(response.ok) return response.json();
+		return response.json().then(error => {
+			const err = new Error('Something gone wrong...');
+			err.data	= error;
+			throw err;
+		});
+	});
+}
+
+sendRequest('GET', requestUrl)
+	.then(data =>  console.log('GET:', data))
+	.catch(error => console.error(error))
+
+const body = {
+	name: 'Andrey',
+	age: 27
+}
+
+sendRequest('POST', requestUrl, body)
+	.then(data => console.log('POST:',data))
+	.then(() => {
+		sendRequest('GET', requestUrl)
+			.then(data =>  console.log('GET:', data))
+			.catch(error => console.error(error))
+	})
+	.catch(error => console.error(error))
+
+	
