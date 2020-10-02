eaf4fb54897458ca9a733216fac91266638ac547
Andrey Fedorovich <a.fedorovich@pixelplex.by>
2020-10-02 23:13:48 +0300

    spaces

--- fetch.js
+++ fetch.js
@@ -1,37 +1,35 @@
 const requestUrl = 'https://jsonplaceholder.typicode.com/users';
 
 function sendRequest(method, url, body = null) {
-	const postParams = method === 'POST' ? {
-		body: JSON.stringify(body),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	} : {};
+  const postParams = method === 'POST' ? {
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  } : {};
 
-	return fetch(url, {
-		method,
-		...postParams
-	}).then(response => {
-		if(response.ok) return response.json();
-		return response.json().then(error => {
-			const err = new Error('Something gone wrong...');
-			err.data	= error;
-			throw err;
-		});
-	});
+  return fetch(url, { method, ...postParams })
+    .then(response => {
+      if(response.ok) return response.json();
+      return response.json().then(error => {
+        const err = new Error('Something gone wrong...');
+        err.data	= error;
+        throw err;
+      });
+    });
 }
 
 sendRequest('GET', requestUrl)
-	.then(data =>  console.log('GET:', data))
-	.catch(error => console.error(error))
+  .then(data =>  console.log('GET:', data))
+  .catch(error => console.error(error))
 
 const body = {
-	name: 'Andrey',
-	age: 27
+  name: 'Andrey',
+  age: 27
 }
 
 sendRequest('POST', requestUrl, body)
-	.then(data => console.log('POST:',data))
-	.catch(error => console.error(error))
+  .then(data => console.log('POST:',data))
+  .catch(error => console.error(error))
 
-	
+  
--- xhr.js
+++ xhr.js
@@ -3,39 +3,36 @@ const requestUrl = 'https://jsonplaceholder.typicode.com/users';
 const xhr = new XMLHttpRequest();
 
 function sendRequest(method, url, body = null) {
-	return new Promise((resolve, reject) => {
-		xhr.open(method, url);
-		xhr.responseType ='json';
-		xhr.setRequestHeader('Content-Type', 'application/json');
-		
-		xhr.onload = () => {
-			if(xhr.status >= 400) {
-				reject(xhr.response);
-				return;
-			}
-			resolve(xhr.response);
-		}
-		
-		xhr.onerror = () => {
-			reject(xhr.response);
-		}
-		
-		xhr.send(JSON.stringify(body));
-	})
+  return new Promise((resolve, reject) => {
+    xhr.open(method, url);
+    xhr.responseType ='json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    xhr.onload = () => {
+      if(xhr.status >= 400) {
+        reject(xhr.response);
+        return
+      }
+      resolve(xhr.response)
+    }
+    
+    xhr.onerror = () => reject(xhr.response)
+    xhr.send(JSON.stringify(body))
+  })
 }
 
 sendRequest('GET', requestUrl)
-	.then(data => console.log(data))
-	.then(()=> {
-		const body = {
-			name: 'Andrey',
-			age: 27
-		}
-		sendRequest('POST', requestUrl, body)
-			.then(data => console.log(data))
-			.catch(error => console.error(error))
-	})
-	.catch(error => console.error(error))
-	
+  .then(data => console.log(data))
+  .then(()=> {
+    const body = {
+      name: 'Andrey',
+      age: 27
+    }
+    sendRequest('POST', requestUrl, body)
+      .then(data => console.log(data))
+      .catch(error => console.error(error))
+  })
+  .catch(error => console.error(error))
+  
 
-		
+    
